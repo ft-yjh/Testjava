648ebd292ef0fd691eb17348a28624aca88361f7
ft-yjh <1943778865@qq.com>
2021-01-05 18:22:46 +0800

    java类的练习

--- test01/src/day02/Student.java
+++ test01/src/day02/Student.java
@@ -1,4 +1,49 @@
 package day02;
 
 public class Student {
+    private String _name;
+    private int _age;
+
+    Student()
+    {
+        System.out.println("这是一个无参的构造方法");
+    }
+    Student(String name, int age)
+    {
+        System.out.println("这是一个有参的构造方法");
+        this._name = name;
+        if (age > 0 && age < 120)
+        {
+            this._age = age;
+        }
+        else
+            System.out.println("输入年龄有误");
+    }
+
+    public void setName(String name)
+    {
+        this._name = name;
+    }
+    public void setAge(int age)
+    {
+        if (age > 0 && age < 120)
+        {
+            this._age = age;
+        }
+        else
+            System.out.println("输入年龄有误");
+    }
+    public String getName()
+    {
+        return this._name;
+    }
+    public int get_age()
+    {
+        return this._age;
+    }
+    public void show()
+    {
+        System.out.println(this._name + "..." + this._age);
+    }
+
 }
--- test01/src/day02/TestStudent.java
+++ test01/src/day02/TestStudent.java
@@ -1,4 +1,19 @@
 package day02;
 
+import java.util.zip.ZipOutputStream;
+
 public class TestStudent {
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.setName("张三");
+        s1.setAge(20);
+        System.out.println(s1.getName());
+        System.out.println(s1.get_age());
+        s1.show();
+        System.out.println("____________________");
+        Student s2 = new Student("李四", -23);
+        System.out.println(s2.getName());
+        System.out.println(s2.get_age());
+        s2.show();
+    }
 }
